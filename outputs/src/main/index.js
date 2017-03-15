dc4aa0c390d0770e0cd19fde528d33ea974143c1
Chris <chris@xappmedia.com>
2017-03-15 17:07:49 -0400

    Initial

---
+++ outputs/src/main/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+//# sourceMappingURL=index.js.map
