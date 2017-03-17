8293495f7efc232a36a6468cc6797e69f5987ac7
Chris <chris@xappmedia.com>
2017-03-17 16:19:46 -0400

    Retrieving creds from environment variables.

--- src/main/controllers/GenerateNameApi.ts
+++ src/main/controllers/GenerateNameApi.ts
@@ -7,12 +7,22 @@ import * as Source from "../models/Source";
 import * as StringUtils from "../utils/StringUtils";
 
 // Fetch the service account key JSON file contents
-var serviceAccount = require("../../../../creds/bespoken-tools-firebase-adminsdk-vwdeq-1b1098346f.json");
+// var serviceAccount = require("../../../../creds/bespoken-tools-firebase-adminsdk-vwdeq-1b1098346f.json");
+const serviceAccount = {
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL.replace(/\\n/g, '\n'),
+}
+
+console.log(serviceAccount);
+
+if (!serviceAccount.private_key || !serviceAccount.client_email) {
+    throw new Error("The FIREBASE_PRIVATE_KEY and FIREBASE_CLIENT_EMAIL environment variables must be provided to log in to Firebase.");
+}
 
 // Initialize the app with a service account, granting admin privileges
 Admin.initializeApp({
-  credential: Admin.credential.cert(serviceAccount),
-  databaseURL: Config.BESPOKEN_TOOLS_FIREBASE_URL
+    credential: Admin.credential.cert(serviceAccount),
+    databaseURL: Config.BESPOKEN_TOOLS_FIREBASE_URL
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
@@ -21,7 +31,7 @@ var db = Admin.database();
 export default function generateUniqueSourceName(name: string): Promise<Source.SourceObj> {
     const newSource: Source.SourceObj = { name: name, secretKey: UUID.v4() };
     return generateName(name, namechecker(), nameGenerator())
-        .then(function(name: string) {
+        .then(function (name: string) {
             newSource.name = name;
             return newSource;
         });
@@ -29,12 +39,12 @@ export default function generateUniqueSourceName(name: string): Promise<Source.S
 
 function namechecker(): (name: string) => Promise<boolean> {
     const sourcesPath = db.ref().child("sources");
-    return function(name: string): Promise<boolean> {
+    return function (name: string): Promise<boolean> {
         // This attempts to read the key at the given source.  If it passes, then the key exists. Else it does not exist and can continue
         return sourcesPath.child(name).once("value")
-            .then(function(result: any) {
+            .then(function (result: any) {
                 return result.exists();
-            }).catch(function(error: Error) {
+            }).catch(function (error: Error) {
                 console.error(error);
                 return false;
             });
@@ -43,7 +53,7 @@ function namechecker(): (name: string) => Promise<boolean> {
 
 function nameGenerator(): (name: string, remaining: number) => string {
     let extraCount = 0;
-    return function(name: string, remaining: number): string {
+    return function (name: string, remaining: number): string {
         // console.info("Generating " + name + " " + remaining);
         if (remaining % 10 === 0) {
             ++extraCount;
