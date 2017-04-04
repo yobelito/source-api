a451e818a441577bbb3ab25deb2adb5c1a941993
Chris <chris@xappmedia.com>
2017-04-04 17:37:03 -0400

    Adding an index test

---
+++ src/test/services/v1/index.test.ts
@@ -0,0 +1,18 @@
+import * as Chai from "chai";
+
+import * as Services from "../../../main/services/v1";
+import GetSourceId from "../../../main/services/v1/GetSourceId";
+import PostLinkSourceToUser from "../../../main/services/v1/PostLinkSourceToUser";
+
+const expect = Chai.expect;
+
+describe("serves/v1/index", function() {
+
+    it("Tests that the export for the GetSourceId is correct.", function() {
+        expect(Services.getSourceId).to.equal(GetSourceId);
+    });
+
+    it("Tests that the PostLinkSourceToUser is correct.", function() {
+        expect(Services.postLinkSourceToUser).to.equal(PostLinkSourceToUser);
+    });
+});
