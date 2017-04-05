604baf45881251681cf9bcc4daf6b36a7ae4dc61
Chris <chris@xappmedia.com>
2017-04-05 16:19:57 -0400

    Correcting dev site

--- src/main/config.ts
+++ src/main/config.ts
@@ -2,5 +2,5 @@ export const APPEND_LENGTH = 5;
 export const GENERATION_FAIL_LIMIT = 100;
 
 export const BESPOKEN_TOOLS_FIREBASE_PROD_URL = "https://bespoken-tools.firebaseio.com/";
-export const BESPOKEN_TOOLS_FIREBASE_DEV_URL = "https://bespoken-tools.firebaseio.com/";
+export const BESPOKEN_TOOLS_FIREBASE_DEV_URL = "https://dev-bespoken-tools.firebaseio.com/";
 export const BESPOKEN_TOOLS_FIREBASE_URL = BESPOKEN_TOOLS_FIREBASE_DEV_URL;
