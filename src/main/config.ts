0957e3082d1668f694eac4e01864098278516814
Chris <chris@xappmedia.com>
2017-04-06 12:57:45 -0400

    Switching to prod

--- src/main/config.ts
+++ src/main/config.ts
@@ -3,4 +3,4 @@ export const GENERATION_FAIL_LIMIT = 100;
 
 export const BESPOKEN_TOOLS_FIREBASE_PROD_URL = "https://bespoken-tools.firebaseio.com/";
 export const BESPOKEN_TOOLS_FIREBASE_DEV_URL = "https://dev-bespoken-tools.firebaseio.com/";
-export const BESPOKEN_TOOLS_FIREBASE_URL = BESPOKEN_TOOLS_FIREBASE_DEV_URL;
+export const BESPOKEN_TOOLS_FIREBASE_URL = BESPOKEN_TOOLS_FIREBASE_PROD_URL;
