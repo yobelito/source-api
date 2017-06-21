7b9b314ca3338fc1b7b4026acc7a304f9557c276
John Kelvie <john.kelvie@gmail.com>
2017-06-21 16:01:01 -0500

    Added firebase key and email as environment variables

--- docker/deployment.sh
+++ docker/deployment.sh
@@ -2,6 +2,7 @@ echo "FIP: $1"
 echo "Branch/Tag: $2"
 echo "Service: $3"
 echo "Env: $4"
+echo "Email: $5"
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 docker build -f docker/deploy/Dockerfile -t bespoken/source-api:$2 .
@@ -14,6 +15,8 @@ docker push bespoken/source-api:$2
     -e SSL_KEY="$SSL_KEY" \
     -e SSL_CERT="$SSL_CERT" \
     -e API_TOKEN=$API_TOKEN \
+    -e FIREBASE_EMAIL=$5 \
+    -e FIREBASE_KEY=$6 \
     --name $3 \
     --size s4 \
     --restart always \
