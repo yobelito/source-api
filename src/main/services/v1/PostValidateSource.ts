2a516e73980e97e3913571a18275b3381b3f7c7b
Chris Ramón <rchristian.ramon@gmail.com>
2017-08-21 17:22:18 -0500

    PostValidateSource: use silent echo dev (#19)

--- src/main/services/v1/PostValidateSource.ts
+++ src/main/services/v1/PostValidateSource.ts
@@ -7,7 +7,7 @@ import * as sdk from "silent-echo-sdk";
 export function postValidateSource(db: Admin.database.Database): (req: Express.Request, res: Express.Response) => Promise<Express.Response> {
     return async function (req: Express.Request, res: Express.Response) {
         const {script, token} = req.body;
-        const silentEchoScript = new sdk.SilentEchoScript(token);
+        const silentEchoScript = new sdk.SilentEchoScript(token, "https://silentecho-dev.bespoken.io/process");
         let validationResult: sdk.ISilentEchoValidatorResult;
         try {
             validationResult = await silentEchoScript.execute(script);
